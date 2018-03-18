3bb54b5d354024b11e2fe02e08ef27e9add2c1a3
Daniel Gonzalez Briseno <sonusbeat@hotmail.com>
2018-03-17 18:44:12 -0700

    Sidebar Modified

--- resources/views/templates/admin-main.blade.php
+++ resources/views/templates/admin-main.blade.php
@@ -14,8 +14,8 @@
         <link rel="stylesheet" href="/css/skins/_all-skins.min.css">
 
         <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+            <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+            <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
         <![endif]-->
 
         <!-- Google Font -->
